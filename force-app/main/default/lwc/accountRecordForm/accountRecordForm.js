0d276f5ffe76e61a85ea423406041c3463b76a83
Alex Trzeciak <trebek08@gmail.com>
2019-09-11 22:16:08 -0700

    show all fields on form

--- force-app/main/default/lwc/accountRecordForm/accountRecordForm.js
+++ force-app/main/default/lwc/accountRecordForm/accountRecordForm.js
@@ -4,8 +4,6 @@ export default class AccountRecordForm extends LightningElement {
     @track
     recordId;
 
-    fields = ['Name', 'Phone', 'Website'];
-
     handleSuccess(event) {
         this.recordId = event.detail.id;
     }
